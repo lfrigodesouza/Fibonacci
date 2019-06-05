84d9c3ebfecda788d8cc06b8121ad259afdda9b5
Lucas Frigo de Souza <lucas.souza@ppi-multitask.com.br>
2019-06-05 08:14:42 -0300

    Implementação Fibonacci

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var i in GetFibonacci().Take(20))
+            {
+                Console.WriteLine(i);   
+            }
+        }
+
+        private static IEnumerable<int> GetFibonacci()
+        {
+            int current = 1, next = 1;
+
+            while(true)
+            {
+                yield return current;
+                next = current + (current = next);
+            }
+        }
+    }
+}
